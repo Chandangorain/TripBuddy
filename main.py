9d08c8494f4566a4fcd565b49d2d448b72c2d719
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-09-15 17:57:22 +0530

    setup done

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+# pip install langgraph langchain langchain-openai langchain-groq langchain-community langchain-tavily psycopg[binary] psycopg_pool python-dotenv tavily-python pip install requests streamlit
+
+# install PostgresSql and create database
+CREATE DATABASE langgraph_memory;  ( or open pgadmin4 and create database there )
+'''
+# LangGraph Multi-Agent Travel Booking System with Long-Term Memory
+
+# main.py
+
+import os
+from typing import TypedDict, Annotated
+import operator
+
+import psycopg
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.postgres import PostgresSaver
+from langchain_core.messages import (
+    AnyMessage,
+    HumanMessage,
+    AIMessage,
+    SystemMessage,
+)
